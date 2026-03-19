51130f7697da6f34531f6fb704fcf9518685aab6
codegeass <denzelgatugu@outlook.com>
2026-03-19 22:34:07 +0300

    Implement global error handling

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,25 +1,52 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile/firebase_options.dart';
 import 'package:mobile/router/app_router.dart';
+import 'package:mobile/services/error_logger/error_logger.dart';
 import 'package:mobile/ui/app/pages/my_app.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load();
+      await dotenv.load();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
 
-  final appRouter = AppRouter();
+      final appRouter = AppRouter();
 
-  runApp(
-    ProviderScope(
-      child: MyApp(appRouter: appRouter),
-    ),
+      runApp(
+        ProviderScope(
+          child: MyApp(appRouter: appRouter),
+        ),
+      );
+    },
+    (error, stackTrace) {
+      if (kDebugMode) {
+        ErrorLoggerService.instance.logError(
+          error,
+          message: 'Error in main:runZonedGuarded',
+          stackTrace: stackTrace,
+        );
+      } else {
+        FlutterError.onError = (errorDetails) {
+          FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+        };
+
+        PlatformDispatcher.instance.onError = (error, stack) {
+          FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+          return true;
+        };
+      }
+    },
   );
 }
