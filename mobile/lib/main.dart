dac6d4fe85b1829c43f2ca260f343f7e3b2dc0c5
codegeass <denzelgatugu@outlook.com>
2026-03-19 22:18:09 +0300

    Initialize Firebase for Android and iOS

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile/firebase_options.dart';
 import 'package:mobile/providers/supabase/supabase_client_provider.dart';
 import 'package:mobile/router/app_router.dart';
 import 'package:mobile/ui/theme/app_theme.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await dotenv.load();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const ProviderScope(child: MyApp()));
 }
